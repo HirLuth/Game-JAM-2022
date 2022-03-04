573ad16c2dcd9811dbbef6fce1666522feba2650
Elythenes <e.dehay@rubika-edu.com>
2022-03-04 23:01:05 +0100

    Mécanique prendre et poser le verre

--- Safépeur/Assets/Scripts/GrabVerre.cs
+++ Safépeur/Assets/Scripts/GrabVerre.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class GrabVerre : MonoBehaviour
 {
     public GameObject player;
+    public GameObject zonePose;
     public bool VerreGrab = false;
+    private float playerDistance;
+    public KeyCode Space = KeyCode.Space;
     
-    void Start()
-    {
-        
-    }
-
-   
     void Update()
     {
+        playerDistance = Mathf.Abs(player.transform.position.x - zonePose.transform.position.x);
         if (VerreGrab == true)
         {
             transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1,
                 player.transform.position.z);
         }
+
+        if (VerreGrab == false)
+        {
+            transform.position = zonePose.transform.position; 
+        }
+
+        if (playerDistance < 3)
+        {
+            if (Input.GetKeyDown(Space) && VerreGrab == true)
+            {
+                
+                VerreGrab = false;
+            }
+        }
+        
     }
 
     public void OnTriggerEnter2D(Collider2D player)
