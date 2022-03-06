48f5eeb6340f8e4cd63e478f4c7cc91b73e17f95
Elythenes <e.dehay@rubika-edu.com>
2022-03-06 13:15:44 +0100

    Un peu de LD

---
+++ Safépeur/Assets/Scripts/SonScreamer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonScreamer : MonoBehaviour
+{
+    public GameObject em;
+    public AudioSource audiosource1;
+    private bool audioBool = true;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (em.sonActivation == true && audioBool == true)
+        {
+            audiosource1.Play();
+            audioBool = false;
+        }*/
+    }
+}
